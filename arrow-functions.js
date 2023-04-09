381273b82d80430240e9f61a3ba6e9649e1a380b
Frangidha <francis.gillis567@gmail.com>
2023-04-09 18:44:00 +0000

    lessons

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,34 @@
  */
 
 // Vanilla JavaScript Function
-
+function addTwoNumbers(a, b) {
+return a + b;
+}
+let sum = addTwoNumbers(3,5)
+console.log(sum)
 // Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => {
+    return a + b;
+    }
+    let sum2 = addTwoNumbers2(5,5)
+    console.log(sum2)
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a, b) => a + b
+
+let sum3 = addTwoNumbers3(6,5)
+
+console.log(sum3)
 
 // Implicit Returns
+const text = message => console.log(message)
 
+text("hello")
+
+const sayHello = () => console.log("hello short")
+
+sayHello();
 // Returning Multiple Lines
+
+ const returnMultipleLines = ( ) => (
+   "xxxx")
+console.log(returnMultipleLines)
---
+++ spread-rest-destructuring.js
@@ -0,0 +1,47 @@
+//spread operator
+
+// No spread operator
+
+let arr1 = [1, 2, 3]
+
+let arr2 = arr1
+
+arr2.push(4)
+
+console.log("Second array:", arr2)
+console.log("First array:", arr1)
+
+// Copying array
+
+let arr3 = [4,5,6]
+let arr4 = [...arr3]
+
+arr4.push(7)
+
+console.log("Third array:", arr3)
+console.log("Fourth array:", arr4)
+
+// Copying object
+
+let obj1 = {a:1, b:2 , c:3}
+
+let obj2 = {...obj1, d:4}
+
+let obj3 = {...obj1, b:5}
+
+console.log("First Object", obj1)
+console.log("Second Object", obj2)
+console.log("Thid Object", obj3)
+
+// Copying part of array or object
+
+let arr5 = [...arr1, {...obj1}, ...arr3, "x","y","z"]
+
+console.log(arr5)
+
+
+//rest operator
+
+const sumAll= (a,b,c) => a + b + c;
+let sum = sumAll(1,2,3)
+console.log('sum:',sum)
